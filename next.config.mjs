b90a4dade141fe6baef63ea0d78292dc762d4bfc
Edgar Morales <desarrolloaplicaciones1981@gmail.com>
2024-06-28 20:33:00 -0400

    Fix ESM and alias issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-/** @type {import('next').NextConfig} */
 import path from 'path';
-const nextConfig = {
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
-  reactStrictMode: true,
 };
 
-export default nextConfig;
-
-
-
+export default config;
